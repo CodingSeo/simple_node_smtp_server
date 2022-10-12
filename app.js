79f3e95f4083cd3874538d72d87314aeab9d705d
서진교 <sjg@gabia.com>
2022-10-12 19:19:19 +0900

    simple node smtp server

---
+++ app.js
@@ -0,0 +1,35 @@
+const SMTPServer = require("smtp-server").SMTPServer;
+const parser = require("mailparser").simpleParser
+
+const app = new SMTPServer({
+    onConnect(session, callback) {
+        console.log(`onConnect ID : ${session.id} remote address : ${session.remoteAddress}`);
+        return callback();
+    },
+    onMailFrom(address, session, callback) {
+        console.log(`onMailFrom ${address.address}`);
+        return callback(); // Accept the address
+    },
+    onRcptTo(address, session, callback) {
+        console.log(`onRcptTo ${address.address}`);
+        return callback(); // Accept the address
+    },
+    onData(stream, session, callback) {
+        parser(stream, {}, (err, parsed) => {
+            if (err)
+                console.log("Error:", err)
+
+            // console.log(parsed.attachments)
+            // console.log(parsed.headers)
+            // console.log(parsed.html)
+            callback();
+        })
+    },
+    onClose(session) {
+        console.log(`onClose ID : ${session.id}`);
+    },
+    secure: false,
+    disabledCommands: ['AUTH']
+});
+
+app.listen(8484)
